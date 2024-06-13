d5871154561bcec42462a4ffcff34f4308ba40ed
Pongsakorn2341 <meepanda2341@gmail.com>
2024-06-13 16:07:23 +0700

    style: text dashboard

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -8,16 +8,19 @@ import { useSession } from "next-auth/react";
 const DashboardPage = () => {
   const session = useSession();
   const isSignedIn = session.status == "authenticated";
-  // TODO : check is valid session ?
 
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>Kryptodian asset allocation</h1>
+        <h1>Kryptodian Portfolio</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <TypewriterComponent
             options={{
-              strings: ["Crypto Currencies", "Realtime Charts"],
+              strings: [
+                "Crypto Currencies",
+                "Realtime Charts",
+                "Build your own",
+              ],
               autoStart: true,
               loop: true,
             }}
